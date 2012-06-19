c3d72c454a3f56e3b819536f0f4f007c0509f1c6
7rans <transfire@gmail.com>
2012-06-19 00:05:13 -0400

    Adjust note about using Facets.

--- lib/ratch/core_ext/facets.rb
+++ lib/ratch/core_ext/facets.rb
@@ -1,6 +1,6 @@
 # I know some people will be deterred by the dependency on Facets b/c they
 # see it as a "heavy" dependency. But really that is far from true, consider
-# the following libs are all that it used.
+# the facet that the following libs are all that it used.
 
 require 'facets/array/not_empty'
 require 'facets/dir/multiglob'            # DEPRECATE: when new #glob is working.
